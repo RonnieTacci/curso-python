6dbfc870b4f3b14434a75e11c39ea248dca9f223
Ronnie Tacci <rlonnierdl@gmail.com>
2026-03-27 21:07:59 -0300

    first commit

---
+++ hello.py
@@ -0,0 +1,12 @@
+print("Hello Word!")
+
+# Variaveis e tipo de dados "basicos"
+
+# Uma variavel é um espaço na memoria onde armazena um valor.
+
+nome = "Ronnie"       #Variavel do tipo "String".
+idade = 25            #Variavel do tipo inteiro (sem casas decimais)
+altura = 1.70         #Variavel do tipo float (com casas decimais)
+dev = True            #Variavel booleana, valores logicos (True e False)
+
+print(f"Meu nome é ", nome, "tenho ", idade, "anos e tenho aproximadamente ", altura)
