5ab010a4ff00a056df58bb81c6891464628e3a41
Ronnie Tacci <rlonnierdl@gmail.com>
2026-03-28 16:10:24 -0300

    Operadores matematicos

---
+++ operadores.py
@@ -0,0 +1,13 @@
+#Operadores matematicos
+
+#Operadores basicos: + , - , *, / , % , **
+# % -> resto de divisão.
+# **-> numero elevado.(potencia)
+
+#Operadores de comparação: == , != , > , < , >= , <=
+
+print(5 % 2) #resto 1 - numero impar
+print(4 ** 2) 
+
+print( 2 == 3) #(==) -> igual - False
+print( 2 != 3) #(!=) -> diferente - True
