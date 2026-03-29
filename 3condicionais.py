887a3fb0ed0e93a2e991436706ff8723c29f0d57
Ronnie Tacci <rlonnierdl@gmail.com>
2026-03-29 19:02:58 -0300

    Exemplo condicionais

---
+++ 3condicionais.py
@@ -0,0 +1,14 @@
+#CONDICIONAIS
+
+'''São estruturas que permitem ao nosso programa tomar decisões
+com base em determinadas condições. O programa executa ações 
+diferentes dependendo de uma situação especifica.'''
+
+#Exemplo:
+
+'''Estou em uma cafeteria e estou com pouco dinheiro.
+O cappucino custa 10 reais, café com leite 7 e o café simples 4.
+
+SE eu tiver 10 reis ou mais (>=), posso pedir cappuccino
+SE eu tiver 7 reais ou mais, possopedir café com leite
+SE NÃO, pede o café simples.'''
