e39b411b857f28b0bc47f7067a6e74bfebcc1bb9
Ronnie Tacci <rlonnierdl@gmail.com>
2026-03-29 19:23:08 -0300

    exemplo

--- 3condicionais.py
+++ 3condicionais.py
@@ -12,3 +12,13 @@ O cappucino custa 10 reais, café com leite 7 e o café simples 4.
 SE eu tiver 10 reis ou mais (>=), posso pedir cappuccino
 SE eu tiver 7 reais ou mais, possopedir café com leite
 SE NÃO, pede o café simples.'''
+
+#Exemplo:  (if, elif e else)
+
+idade = int(input("Informe sua idade: "))
+
+if idade >= 18:
+    print("Permissão para jogar lol")
+
+else:
+    print("Caiu na lei Felca, volte quando tiver idade adequada. ")
