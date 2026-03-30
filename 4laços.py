c0020ed210f8694601b994a4dd4bd12020477e83
Ronnie Tacci <rlonnierdl@gmail.com>
2026-03-30 14:33:30 -0300

    for e while

--- 4laços.py
+++ 4laços.py
@@ -7,3 +7,44 @@ ate que uma condição seja atingida. '''
 
 #for -> Quando sabemos quantas vezes queremos repetir algo.
 #while -> Quando queremos repetir algo ate que uma condição se torne falsa.
+
+
+#Estrutura for
+
+'''for VARIAVEL in SEQUENCIA():
+    CODIGO A SER REPETIDO'''
+
+'''for '''
+
+for numero in range(1,11):
+    print(numero)
+
+compras = ["arroz", "feijão", "batata", "frango"]
+
+for itens in compras:
+    print(f"Comprar: {itens}")
+
+palavra = "Ronnie"
+
+for letra in palavra:
+    print(letra)
+
+'''while CONDIÇÃO:
+    CODIGO A SER REPETIDO'''
+
+#Cuidado com loops infinitos!!   Para encerrar o looping no Windows: Ctrl + C
+
+contador = 10
+
+while contador > 0:
+    print(contador)
+    contador -= 1  #Diminui 1 do contador a cada repetição 
+
+
+senha_correta = "1234"
+senha = ""
+
+while senha != senha_correta:
+    senha = input("Digite a senha: ")
+
+print("Acesso liberado")
