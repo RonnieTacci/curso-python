1fe149bea6c6d9f817545ab5276e30c6eb93ff31
Ronnie Tacci <rlonnierdl@gmail.com>
2026-03-30 13:50:32 -0300

    laços

---
+++ 4laços.py
@@ -0,0 +1,9 @@
+# LAÇOS DE REPETIÇÃO
+
+'''Os laços de repetição são usados para executar um bloco de codigo varias vezes
+ate que uma condição seja atingida. '''
+
+#Python tem dois tipos principais de laços:
+
+#for -> Quando sabemos quantas vezes queremos repetir algo.
+#while -> Quando queremos repetir algo ate que uma condição se torne falsa.
