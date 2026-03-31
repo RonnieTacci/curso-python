c926e9cafbee69e54e83048922db498472d281c4
Ronnie Tacci <rlonnierdl@gmail.com>
2026-03-31 15:39:37 -0300

    listas

---
+++ 5listas.py
@@ -0,0 +1,28 @@
+# LISTAS E TUPLAS
+
+#Tipos de dados que armazenam multiplos valores
+
+'''LISTAS:
+
+- Modificavel(pode adicionar, remover e alterar valores)
+- Mais lenta
+- Boa para quando precisamos modificar dados'''
+
+'''Tuplas:
+
+- Não pode ser modificada/alterada apos a criação!
+- Mais rapida
+- Utilizada para dados que não podem ser altarados'''
+
+#Lista
+#Definida entre colchetes [] e pode armazenar diferentes tipos de dados
+
+frutas = ["maçã", "banana", "laranja"]
+numeros = [1, 2, 3, 4, 5]
+misturados = ["python", 3.14, True]
+
+print(frutas)
+print(frutas[0])
+print(frutas[1])
+print(frutas[2])
+print(frutas[-1])
