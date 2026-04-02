8d78a786f474bbfb1723b3460c25928cf297d81b
Ronnie Tacci <rlonnierdl@gmail.com>
2026-04-02 19:18:59 -0300

    Projeto01

---
+++ projeto01.py
@@ -0,0 +1,12 @@
+#Jogo de adivinhação
+
+''' No jogo, o usuario precisa advinhar um numero secreto.
+    Ele pode tentar varias vezes ate acertar.'''
+
+numero_secreto = 10
+tentativa = 0
+
+while tentativa != numero_secreto:
+    tentativa = float(input("Tente adivinhar um numero de 1 a 10:"))
+
+print("Adivinhou o numero!")
