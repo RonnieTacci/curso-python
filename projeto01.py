6b600ad567b70effd97a69ac842aff0d47bc9abf
Ronnie Tacci <rlonnierdl@gmail.com>
2026-04-03 23:08:42 -0300

    Projeto01

--- projeto01.py
+++ projeto01.py
@@ -3,18 +3,16 @@
 ''' No jogo, o usuario precisa advinhar um numero secreto.
     Ele pode tentar varias vezes ate acertar.'''
 
-numero_secreto = 10
+numero_secreto = 36
 tentativa = 0
 
 while tentativa != numero_secreto:
-    tentativa = float(input("Tente adivinhar um numero de 1 a 10:"))
+    tentativa = float(input("Tente adivinhar um numero de 1 a 40:"))
 
+    if tentativa < numero_secreto:
+        print("O numero secreto é maior.")
 
-
-
-
-
-
-
+    else:
+        print("O numero é menor.")    
 
 print("Adivinhou o numero!")
