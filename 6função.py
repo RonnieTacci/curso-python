e951c7cdd02c5249c8f1351fc9e48aa0be4389b6
Ronnie Tacci <rlonnierdl@gmail.com>
2026-04-05 20:08:23 -0300

    Ex funções

--- 6função.py
+++ 6função.py
@@ -10,3 +10,20 @@ e apenas chamamos sempre que necessario.'''
 Em vez de repetir a mesma conta varias vezes, criamos uma função
 chamada calcular_imposto() e usamos quando precisar.'''
 
+def saudacao (nome):
+    print(f"Olá, {nome}! Seja bem-vindo!")
+
+saudacao("Ronnie")
+
+def somar (a, b):
+    return a + b
+
+resultado = somar(10, 4)
+print(f"A soma é {resultado}")
+
+def calcular_media(n1, n2, n3):
+    media = (n1+ n2+ n3) / 3
+    return media
+
+resultado_media = calcular_media(8, 9, 4)
+print(f"A medía é {resultado_media}")
