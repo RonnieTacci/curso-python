da4a08fe7333d325407ac3bf8d3964b57b3bd82d
Ronnie Tacci <rlonnierdl@gmail.com>
2026-04-04 22:55:56 -0300

    projeto 02

---
+++ projeto02.py
@@ -0,0 +1,15 @@
+#Simular um caixa eletronico
+
+'''O usuario tem um saldo inicial de 500 reais e pode
+sacar dinheiro ate zerar o saldo ou encerrar.'''
+
+saldo = 500
+
+while saldo > 0:
+    saque= int(input("Informe o valor do saque: "))
+
+    if saque > saldo:
+        print("Saldo insuficiente! Saque não efetuado.")
+    else:
+        saldo -= saque
+        print(f"Saque efetuado.Novo saldo:{saldo}")
