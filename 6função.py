adcc2cd12241a1a895399ed9a49a9f0e674784e8
Ronnie Tacci <rlonnierdl@gmail.com>
2026-04-05 19:54:06 -0300

    O QUE É FUNÇÃO

---
+++ 6função.py
@@ -0,0 +1,12 @@
+#Funções
+
+'''Funções são blocos de codigos reutilizaveis que realizam uma tarefa
+espeficica.Em vez de escrever o mesmo codigo varias vezes, criamos uma função
+e apenas chamamos sempre que necessario.'''
+
+#Exemplo do dia a dia
+
+'''Calcular o imposto de varios produtos em uma loja.
+Em vez de repetir a mesma conta varias vezes, criamos uma função
+chamada calcular_imposto() e usamos quando precisar.'''
+
