18bc391958295d84db51a33ed79f25f28998125e
Ronnie Tacci <rlonnierdl@gmail.com>
2026-04-04 23:01:51 -0300

    Projeto 02

--- projeto02.py
+++ projeto02.py
@@ -6,10 +6,15 @@ sacar dinheiro ate zerar o saldo ou encerrar.'''
 saldo = 500
 
 while saldo > 0:
-    saque= int(input("Informe o valor do saque: "))
+    saque= int(input("Informe o valor do saque ou digite 0 para sair. "))
+
+    if saque == 0:
+        break
 
     if saque > saldo:
         print("Saldo insuficiente! Saque não efetuado.")
     else:
         saldo -= saque
         print(f"Saque efetuado.Novo saldo:{saldo}")
+
+print("Operação finalizada.")
