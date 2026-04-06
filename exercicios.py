cc4794e31ebf9a2ce7e0a8d86ac0fbd81785502a
Ronnie Tacci <rlonnierdl@gmail.com>
2026-04-06 18:44:51 -0300

    Logica

---
+++ exercicios.py
@@ -0,0 +1,17 @@
+def multiply(a, b):
+    return a * b
+
+def repeat_str(repeat, string):
+    return repeat * string
+
+def even_or_odd(number):
+    if number % 2 == 0:
+        return "Par"
+    
+    return "Impar"
+
+def opposite(num):
+    return num * -1
+
+def remove_char(L):
+    return L[1:-1]
